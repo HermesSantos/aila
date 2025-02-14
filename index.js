101dbbd5363abd4e35caca5f0f616745874a45ee
HermesSantos <hermessantos995@gmail.com>
2025-02-14 11:20:17 -0300

    Configura dotenv para carregar variáveis de ambiente corretamente

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
-
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import {generateNewCommitMessage} from './functions/generateNewCommitMessage.js'
 
-dotenv.config()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 generateNewCommitMessage()
 
