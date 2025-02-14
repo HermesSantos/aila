07747cb67a533ec54e60644626c3a0c37a8e7aef
HermesSantos <hermessantos995@gmail.com>
2025-02-14 15:28:19 -0300

    Corrige verificação da chave da API Gemini

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-if(!process.env.API_KEY_TEST) {
+if(!process.env.API_KEY) {
   getGeminiApiKey()
 } else {
   generateNewCommitMessage()
